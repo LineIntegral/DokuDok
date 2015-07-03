898547a2a2354f5dd6e7ead7e36c15d8e9b7d04c
alieksi <eksi@gyte.edu.tr>
2015-07-03 15:47:50 +0300

    something good happened

--- viewer.php
+++ viewer.php
@@ -1,6 +1,7 @@
 <?php
 //need to work on it
 include('image.php');
+include('config.php');
 
 class Viewer
 {
@@ -13,6 +14,22 @@ class Viewer
 		$this->IMGMaker = new PDFtoIMG($pdfName);
 		$this->currentPageN = $currentPageN;
 		$this->pdfName = $pdfName;
+	}
+
+	public static function titleToDocName($title)
+	{
+		$query = "SELECT docname,title from books";
+		$conn = new Config();
+		$result = $conn->getConnection()->query($query);
+
+		if($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				if($row["title"] == $title)
+					return $row["docname"];
+			}
+		}
 	}
 
 	public function hasNext()
@@ -39,13 +56,13 @@ class Viewer
 
 if(isset($_POST['next']))
 {
-	$viewer = new Viewer("books/".$_POST['bookName'], $_POST['pageNo']);
+	$viewer = new Viewer("books/".Viewer::titleToDocName($_POST['bookName']), $_POST['pageNo']);
 	$x = $viewer->nextPageN();
 	echo '<center><img src='.$x.' width="500" height="500" >';
 	echo '<br/><br/>';
 	echo '<form action="" method="post">';
 	echo '<input type="hidden" name="pageNo" value='.$viewer->getCurrentPage().'>';
-	echo '<input type="hidden" name="bookName" value='.$_POST['bookName'].'>';
+	echo '<input type="hidden" name="bookName" value="'.$_POST['bookName'].'"">';
 	if($viewer->getCurrentPage() > 1) echo '<input type="submit" name="previous" value="Previous">';
 	echo '<input type="submit" name="next" value="Next">';
 	echo '</form></center>';
@@ -53,13 +70,13 @@ if(isset($_POST['next']))
 elseif(isset($_POST['previous']))
 {
 	Viewer::$currentPageN = $_POST['pageNo']-1;
-	$viewer = new Viewer("books/".$_POST['bookName'], Viewer::$currentPageN);
+	$viewer = new Viewer("books/".Viewer::titleToDocName($_POST['bookName']), Viewer::$currentPageN);
 	$x = $viewer->previousPageN();
 	echo '<center><img src='.$x.' width="500" height="500" >';
 	echo '<br/><br/>';
 	echo '<form action="" method="post">';
 	echo '<input type="hidden" name="pageNo" value='.Viewer::$currentPageN.'>';
-	echo '<input type="hidden" name="bookName" value='.$_POST['bookName'].'>';
+	echo '<input type="hidden" name="bookName" value="'.$_POST['bookName'].'">';
 	if($viewer->getCurrentPage() > 1) echo '<input type="submit" name="previous" value="Previous">';
 	echo '<input type="submit" name="next" value="Next">';
 	echo '</form></center>';
